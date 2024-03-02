0569755757a02a64b81d1d199103ab99eee16393
jwl-7 <lucidrainbows@gmail.com>
2024-03-02 12:23:00 -0500

    Change base for github-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
     plugins: [react()],
+    base: "https://jwl-7.github.io/leetcode-cheatsheet/",
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
