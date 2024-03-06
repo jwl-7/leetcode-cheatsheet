b7ce8263a72d27249d7de533e9c92d5c41cbbcd0
jwl-7 <lucidrainbows@gmail.com>
2024-03-06 06:57:48 -0500

    Simplify implementation

--- src/code/python/data_structures/hash_map.py
+++ src/code/python/data_structures/hash_map.py
@@ -1,16 +1,19 @@
+from typing import Any
+
+
 class HashMap:
     def __init__(self) -> None:
-        self.bucket = [None] * 100000
+        self.size = 100000
+        self.bucket = [None] * self.size
 
-    def put(self, key: int, value: int) -> None:
-        hash_key = hash(key) % 100000
-        self.bucket[hash_key] = value
+    def _hash(self, key: int) -> int:
+        return hash(key) % self.size
 
-    def get(self, key: int) -> int:
-        hash_key = hash(key) % 100000
-        value = self.bucket[hash_key]
-        return value if value is not None else -1
+    def __setitem__(self, key: int, value: Any) -> None:
+        self.bucket[self._hash(key)] = value
 
-    def remove(self, key: int) -> None:
-        hash_key = hash(key) % 100000
-        self.bucket[hash_key] = None
+    def __getitem__(self, key: int) -> Any:
+        return self.bucket[self._hash(key)]
+
+    def __delitem__(self, key: int) -> None:
+        self.bucket[self._hash(key)] = None
