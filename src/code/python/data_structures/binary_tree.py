8bffccd8aaa2c69305c424caa5d50aae1aa28a08
jwl-7 <lucidrainbows@gmail.com>
2024-03-06 08:47:03 -0500

    Refactor

--- src/code/python/data_structures/binary_tree.py
+++ src/code/python/data_structures/binary_tree.py
@@ -1,34 +1,24 @@
+from typing import Any
+
+
 class TreeNode:
-    def __init__(self, data) -> None:
+    def __init__(self, data: Any) -> None:
         self.data = data
         self.left = None
         self.right = None
+
 
 class BinaryTree:
     def __init__(self) -> None:
         self.root = None
 
-    def __repr__(self) -> str:
-        return 'Empty tree' if not self.root else self._repr_tree(self.root, '', True)
-
-    def _repr_tree(self, node, prefix: str, is_left: bool) -> str:
-        if node is None:
-            return ''
-
-        result = ''
-        result += self._repr_tree(node.right, prefix + ('│   ' if is_left else '    '), False)
-        result += prefix + ('└── ' if is_left else '┌── ') + str(node.data) + '\n'
-        result += self._repr_tree(node.left, prefix + ('    ' if is_left else '│   '), True)
-
-        return result
-
-    def insert(self, data) -> None:
+    def insert(self, data: Any) -> None:
         if not self.root:
             self.root = TreeNode(data)
         else:
             self.insert_node(self.root, data)
 
-    def insert_node(self, node, data) -> TreeNode:
+    def insert_node(self, node: TreeNode | None, data: Any) -> TreeNode:
         if not node:
             return TreeNode(data)
 
@@ -40,3 +30,17 @@ class BinaryTree:
             node.left = self.insert_node(node.left, data)
 
         return node
+
+    def __repr__(self) -> str:
+        return 'Empty tree' if not self.root else self._print_tree(self.root, '', True)
+
+    def _print_tree(self, node: TreeNode | None, prefix: str, is_left: bool) -> str:
+        if node is None:
+            return ''
+
+        result = ''
+        result += self._print_tree(node.right, prefix + ('│   ' if is_left else '    '), False)
+        result += prefix + ('└── ' if is_left else '┌── ') + str(node.data) + '\n'
+        result += self._print_tree(node.left, prefix + ('    ' if is_left else '│   '), True)
+
+        return result
--- src/code/python/data_structures/trie.py
+++ src/code/python/data_structures/trie.py
@@ -7,26 +7,6 @@ class Trie:
     def __init__(self) -> None:
         self.root = TrieNode()
 
-    def __repr__(self) -> str:
-        return 'Trie:\n' + self._print_trie(self.root)
-
-    def _print_trie(self, node: TrieNode, level: int=0, prefix: str='') -> str:
-        output = ''
-        prefix_str = '    ' * level + prefix
-
-        if not node:
-            return output
-
-        if node.is_word:
-            output += prefix_str + ' ├─ ' + '(*)' + '\n'
-        for i, (char, child_node) in enumerate(node.children.items()):
-            is_last = i == len(node.children) - 1
-            marker = '└─ ' if is_last else '├─ '
-            output += prefix_str + marker + char + '\n'
-            output += self._print_trie(child_node, level + 1, '    │' if not is_last else '     ')
-
-        return output
-
     def build(self, words: list[str]) -> None:
         for word in words:
             self.insert(word)
@@ -70,3 +50,23 @@ class Trie:
             words.extend(self.collect_words(child_node, prefix + char))
 
         return words
+
+    def __repr__(self) -> str:
+        return 'Trie:\n' + self._print_trie(self.root)
+
+    def _print_trie(self, node: TrieNode, level: int=0, prefix: str='') -> str:
+        output = ''
+        prefix_str = '    ' * level + prefix
+
+        if not node:
+            return output
+
+        if node.is_word:
+            output += prefix_str + ' ├─ ' + '(*)' + '\n'
+        for i, (char, child_node) in enumerate(node.children.items()):
+            is_last = i == len(node.children) - 1
+            marker = '└─ ' if is_last else '├─ '
+            output += prefix_str + marker + char + '\n'
+            output += self._print_trie(child_node, level + 1, '    │' if not is_last else '     ')
+
+        return output
