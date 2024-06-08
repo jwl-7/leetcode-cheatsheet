2c4b44475b1ceda8da9a910175e4f39e704df7fe
jwl-7 <lucidrainbows@gmail.com>
2024-06-08 07:48:20 -0400

    Fix prefix sum

--- src/code/java/array/prefix_sum.java
+++ src/code/java/array/prefix_sum.java
@@ -1,10 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
 public int[] fn(int[] arr) {
     int n = arr.length;
     int[] prefix = new int[n];
-    prefix.add(arr[0]);
+    prefix[0] = arr[0];
 
     for (int i = 1; i < n; i++) {
-        prefix.add(prefix.get(i - 1) + arr[i]);
+        prefix[i] = prefix[i - 1] + arr[i];
     }
 
     return prefix;
