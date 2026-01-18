910f9b17b83a24ecf61ccecde697822accf238d2
jwl-7 <lucidrainbows@gmail.com>
2026-01-18 11:05:40 -0500

    update clsx

--- src/utils/clsx.tsx
+++ src/utils/clsx.tsx
@@ -1,3 +1,22 @@
-export default function clsx(...classes: (string | undefined | null | false)[]): string {
-    return classes.filter(Boolean).join(' ')
+type ClassValue = string | number | boolean | undefined | null | { [key: string]: any } | ClassValue[]
+
+export default function clsx(...inputs: ClassValue[]): string {
+    let str = ''
+    for (const input of inputs) {
+        if (!input) continue
+        if (typeof input === 'string' || typeof input === 'number') {
+            str += (str && ' ') + input
+        } else if (Array.isArray(input)) {
+            const inner = clsx(...input)
+            if (inner) str += (str && ' ') + inner
+        } else if (typeof input === 'object') {
+            for (const key in input) {
+                if (input[key]) {
+                    str += (str && ' ') + key
+                }
+            }
+        }
+    }
+
+    return str
 }
